0c998404f9d17ed5eda080405519acd6cd08cc05
Antony <97451137+TonybynMp4@users.noreply.github.com>
2024-02-28 23:07:35 +0100

    feat: player_vehicles should be in qbx_vehicles, DEFAULT NULL & id as FK

--- vehshop.sql
+++ vehshop.sql
@@ -1,35 +1,12 @@
-CREATE TABLE IF NOT EXISTS `player_vehicles` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `license` varchar(50) DEFAULT NULL,
-    `citizenid` varchar(50) DEFAULT NULL,
-    `vehicle` varchar(50) DEFAULT NULL,
-    `hash` varchar(50) DEFAULT NULL,
-    `mods` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-    `plate` varchar(15) NOT NULL,
-    `fakeplate` varchar(50) DEFAULT NULL,
-    `garage` varchar(50) DEFAULT 'pillboxgarage',
-    `fuel` int(11) DEFAULT 100,
-    `engine` float DEFAULT 1000,
-    `body` float DEFAULT 1000,
-    `state` int(11) DEFAULT 1,
-    `depotprice` int(11) NOT NULL DEFAULT 0,
-    `drivingdistance` int(50) DEFAULT NULL,
-    `status` text DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `plate` (`plate`),
-	FOREIGN KEY (`citizenid`) REFERENCES `players` (`citizenid`) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY  (`license`) REFERENCES `players` (`license`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
 CREATE TABLE IF NOT EXISTS `vehicle_financing` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `citizenid` varchar(50) DEFAULT NULL,
+    `vehicleId` int(11) NOT NULL,
+    `citizenid` varchar(50) NOT NULL,
     `plate` varchar(15) NOT NULL,
-    `balance` int(11) NOT NULL DEFAULT 0,
-    `paymentamount` int(11) NOT NULL DEFAULT 0,
-    `paymentsleft` int(11) NOT NULL DEFAULT 0,
-    `financetime` int(11) NOT NULL DEFAULT 0,
-    PRIMARY KEY (`id`),
+    `balance` int(11) DEFAULT NULL,
+    `paymentamount` int(11) DEFAULT NULL,
+    `paymentsleft` int(11) DEFAULT NULL,
+    `financetime` int(11) DEFAULT NULL,
+    FOREIGN KEY `vehicleId` (`vehicleId`) REFERENCES `player_vehicles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY `plate` (`plate`) REFERENCES `player_vehicles` (`plate`) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY `citizenid` (`citizenid`) REFERENCES `players` (`citizenid`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
