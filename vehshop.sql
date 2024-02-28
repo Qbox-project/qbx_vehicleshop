5a9cdc19f6feb5fc22c04c0550dc2411a7904909
Antony <97451137+TonybynMp4@users.noreply.github.com>
2024-02-28 08:10:48 +0100

    feat: extract financing to it's own table

--- vehshop.sql
+++ vehshop.sql
@@ -16,23 +16,20 @@ CREATE TABLE IF NOT EXISTS `player_vehicles` (
     `drivingdistance` int(50) DEFAULT NULL,
     `status` text DEFAULT NULL,
     PRIMARY KEY (`id`),
-    KEY `plate` (`plate`),
-    KEY `citizenid` (`citizenid`),
-    KEY `license` (`license`)
-) ENGINE=InnoDB AUTO_INCREMENT=1;
+    UNIQUE KEY `plate` (`plate`),
+	FOREIGN KEY (`citizenid`) REFERENCES `players` (`citizenid`) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY  (`license`) REFERENCES `players` (`license`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-ALTER TABLE `player_vehicles`
-ADD UNIQUE INDEX UK_playervehicles_plate (plate);
-
-ALTER TABLE `player_vehicles`
-ADD CONSTRAINT FK_playervehicles_players FOREIGN KEY (citizenid)
-REFERENCES `players` (citizenid) ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE `player_vehicles`
-ADD COLUMN `balance` int(11) NOT NULL DEFAULT 0;
-ALTER TABLE `player_vehicles`
-ADD COLUMN `paymentamount` int(11) NOT NULL DEFAULT 0;
-ALTER TABLE `player_vehicles`
-ADD COLUMN `paymentsleft` int(11) NOT NULL DEFAULT 0;
-ALTER TABLE `player_vehicles`
-ADD COLUMN `financetime` int(11) NOT NULL DEFAULT 0;
+CREATE TABLE IF NOT EXISTS `vehicle_financing` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `citizenid` varchar(50) DEFAULT NULL,
+    `plate` varchar(15) NOT NULL,
+    `balance` int(11) NOT NULL DEFAULT 0,
+    `paymentamount` int(11) NOT NULL DEFAULT 0,
+    `paymentsleft` int(11) NOT NULL DEFAULT 0,
+    `financetime` int(11) NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY `plate` (`plate`) REFERENCES `player_vehicles` (`plate`) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY `citizenid` (`citizenid`) REFERENCES `players` (`citizenid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
