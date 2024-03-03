71ada2ba31ce314526111249282163c329b7d7df
Antony <97451137+TonybynMp4@users.noreply.github.com>
2024-03-04 00:37:51 +0100

    use TinyInt

--- migrate.sql
+++ migrate.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS `vehicle_financing` (
     `vehicleId` int(11) NOT NULL,
     `balance` int(11) DEFAULT NULL,
     `paymentamount` int(11) DEFAULT NULL,
-    `paymentsleft` int(11) DEFAULT NULL,
+    `paymentsleft` tinyint(4) DEFAULT NULL,
     `financetime` int(11) DEFAULT NULL,
     PRIMARY KEY (`vehicleId`),
-    FOREIGN KEY `vehicleId` (`vehicleId`) REFERENCES `player_vehicles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY `vehicleId` (`vehicleId`) REFERENCES `player_vehicles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO vehicle_financing (vehicleId, balance, paymentamount, paymentsleft, financetime)
