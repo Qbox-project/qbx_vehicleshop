af0796659e168e04c8f3af6807b1558a716333c5
Antony <97451137+TonybynMp4@users.noreply.github.com>
2024-03-29 15:18:37 +0100

    Update vehshop.sql

--- vehshop.sql
+++ vehshop.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS `vehicle_financing` (
     `paymentsleft` int(11) DEFAULT NULL,
     `financetime` int(11) DEFAULT NULL,
     PRIMARY KEY (`vehicleId`),
-    FOREIGN KEY `vehicleId` (`vehicleId`) REFERENCES `player_vehicles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE),
+    FOREIGN KEY `vehicleId` (`vehicleId`) REFERENCES `player_vehicles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE)
     ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
