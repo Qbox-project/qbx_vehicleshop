11da1d29274e7801f74907cfaef805ee6659ba32
Swellington Soares <qb.corebr@gmail.com>
2024-10-03 13:06:23 -0300

    fix test driver state bag key name

--- client/main.lua
+++ client/main.lua
@@ -295,8 +295,8 @@ local function startTestDrive(vehModel)
 end
 
 lib.onCache('vehicle', function(value)
-    if value or not LocalPlayer.state.inTestDrive then return end
-    LocalPlayer.state:set('inTestDrive', nil, true)
+    if value or not LocalPlayer.state.isInTestDrive then return end    
+    LocalPlayer.state:set('isInTestDrive', nil, true)
 end)
 
 ---@param vehModel string
